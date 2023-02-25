ecf5755c70a37b75f68fb482363893767a36b42a
João Pedro Barbosa Medeiros <jpedrobmedeiros@gmail.com>
2023-02-25 17:11:22 -0500

    Adicionados argumentos n'ao obrigatórios ao contrutor da classe Pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 
 class Pokemon:
-    def __init__(self, tipo, especie):
+    def __init__(self, tipo, especie, nome=None, level=1):
         self.tipo = tipo
         self.especie = especie
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
+        self.level = level
 
     def __str__(self):
-        return f"{self.especie} ({self.tipo})"
+        return f"{self.nome} ({self.level})"
 
     def atacar(self, pokemon):
         print(f"{self} atacou {pokemon}!")
 
-meuPokemon = Pokemon("Água", "Squirtle")
+meuPokemon = Pokemon("Água", "Squirtle", "Taturu", "50")
 pokemonAmigo = Pokemon("Planta", "Bulbasaur")
+
+print(meuPokemon)
+print(pokemonAmigo)
 
 meuPokemon.atacar(pokemonAmigo)
 pokemonAmigo.atacar(meuPokemon)
